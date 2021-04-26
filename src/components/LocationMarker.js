c3414bd8c2e589e984afbd0891945b337e67a48e
PhonVanna <phonvanna_author@yahoo.com>
2021-04-27 00:24:36 +0700

    pass the api data to map

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -3,11 +3,11 @@ import { Icon } from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 
 const LocationMarker = ({ lat, lng, onClick }) => {
-    return (
-        <div className="location-markeer" onClick={onClick}>
-            <Icon icon={locationIcon} className="location-icon" />
-        </div>
-    )
+  return (
+    <div className="location-markeer" onClick={onClick}>
+      <Icon icon={locationIcon} className="location-icon" />
+    </div>
+  )
 }
 
 export default LocationMarker
--- src/components/Map.js
+++ src/components/Map.js
@@ -2,8 +2,19 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
+const Map = ({ eventData, center, zoom }) => {
+  const markers = eventData.map((ev) => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <LocationMarker
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+        />
+      )
+    }
+    return null
+  })
 
-const Map = ({ center, zoom }) => {
   return (
     <div className="map">
       <GoogleMapReact
@@ -11,7 +22,7 @@ const Map = ({ center, zoom }) => {
         defaultCenter={center}
         defaultZoom={zoom}
       >
-          <LocationMarker lat={center.lat} lng={center.lng} />
+        {markers}
       </GoogleMapReact>
     </div>
   )
