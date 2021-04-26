dbb5663d93b114475d3ecc4912ae25833accca78
PhonVanna <phonvanna_author@yahoo.com>
2021-04-27 00:08:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import Map from './components/Map'
+import Loader from './components/Loader'
+import './App.css'
+
+function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const  res = await fetch("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events")
+      // event is from api
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+
+    // console.log(eventData)
+  }, [])
+
+  return (
+    <div className="App">
+      { !loading ? <Map eventData={eventData} /> : <Loader /> } 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import spinner from './200.gif'
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <img src={spinner} alt="Loading" />
+            <h1>Fetching</h1>
+        </div>
+    )
+}
+
+export default Loader
